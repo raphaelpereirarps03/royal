a66abfc91b18fc03fc648349a4333158baab1344
Raphael Pereira de Souza <89276182+raphaelpereirarps03@users.noreply.github.com>
2024-08-23 12:33:57 -0300

    arrumando algumas coisas da estrutura

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('togglePassword').addEventListener('click', function () {
-    const passwordField = document.getElementById('password');
-    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordField.setAttribute('type', type);
-});
