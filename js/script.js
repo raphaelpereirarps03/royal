40df322455531912247f0721120b092cdb80b37c
lucascarv <lucascarvalhio077@gmail.com>
2024-08-26 02:05:10 -0300

    aperfeiçoando mais o códido-e-tornando-mais-limpo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,51 @@
-document.getElementById('togglePassword').addEventListener('click', function () {
-    const passwordField = document.getElementById('password');
-    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordField.setAttribute('type', type);
-});
+//Remover o texto do placeholder quando o usuário clicar no input
+
+const inputLocal = document.querySelector('#inputLocal')
+const inputRetirada = document.querySelector('#inputLocalRetirada')
+const inputData = document.querySelector('#inputData')
+const inputDataRetirada = document.querySelector('#inputDataRetirada')
+
+
+//Input local
+inputLocal.addEventListener('focus', function(){
+    inputLocal.placeholder = ""
+})
+
+inputLocal.addEventListener('blur', function(){
+    if (inputLocal.value === ""){
+        inputLocal.placeholder = "Local"
+    }
+})
+
+//Input Local de retirada
+inputRetirada.addEventListener('focus', function(){
+    inputRetirada.placeholder = ""
+})
+
+inputRetirada.addEventListener('blur', function(){
+    if (inputRetirada.value === ""){
+        inputRetirada.placeholder = "Retirada"
+    }
+})
+
+//Input data
+inputData.addEventListener('focus', function(){
+    inputData.placeholder = ""
+})
+
+inputData.addEventListener('blur', function(){
+    if (inputData.value === ""){
+        inputData.placeholder = "dd/mm/yyyy"
+    }
+})
+
+//Input data de retirada
+inputDataRetirada.addEventListener('focus', function(){
+    inputDataRetirada.placeholder = ""
+})
+
+inputDataRetirada.addEventListener('blur', function(){
+    if (inputDataRetirada.value === ""){
+        inputDataRetirada.placeholder = "dd/mm/yyyy"
+    }
+})
